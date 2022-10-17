e565ffbb09f3def4f9492fef5f7a8c1c85307020
ruampimentel <ruampimentel@gmail.com>
2022-10-16 21:42:51 -0400

    first time working scratch

---
+++ 00_script.R
@@ -0,0 +1,9 @@
+
+datadrivencv::use_datadriven_cv(
+  full_name = "Ruam P. F. A. Pimentel",
+  data_location = "https://docs.google.com/spreadsheets/d/1HDcEn-lU49vdcht96f2sQXuA2b5g2J84PipYuTVBU7o/edit?usp=sharing",
+  pdf_location = "https://github.com/ruampimentel/Pimentel_CV/Pimentel_cv.pdf",
+  html_location = "ruampimentel.me/cv/",
+  source_location = "https://github.com/ruampimentel/Pimentel_CV",
+  use_network_logo = TRUE # turn it false later and use this space for other stuff
+)
