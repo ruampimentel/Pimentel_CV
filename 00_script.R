f9f22d02afdc4da2e469555a219481722c4714b5
Ruam Pimentel <74716041+ruampimentel@users.noreply.github.com>
2023-02-10 14:40:55 -0500

    First update (really learning it)

--- 00_script.R
+++ 00_script.R
@@ -1,9 +1,25 @@
+# install.packages("devtools")
+# remotes::install_local("datadrivencv-master.zip")
+# remotes::install_github("mitchelloharawild/icons", force = T)
+library(icons)
+library(datadrivencv)
+# download_academicons()
+# download_bioicons()
+# download_fontawesome()
+# download_google_material()
+# download_ionicons()
+# download_octicons()
+# download_simple_icons()
 
 datadrivencv::use_datadriven_cv(
   full_name = "Ruam P. F. A. Pimentel",
-  data_location = "https://docs.google.com/spreadsheets/d/1HDcEn-lU49vdcht96f2sQXuA2b5g2J84PipYuTVBU7o/edit?usp=sharing",
+  data_location = "data/",
   pdf_location = "https://github.com/ruampimentel/Pimentel_CV/Pimentel_cv.pdf",
   html_location = "ruampimentel.me/cv/",
   source_location = "https://github.com/ruampimentel/Pimentel_CV",
   use_network_logo = TRUE # turn it false later and use this space for other stuff
 )
+
+
+
+# use_csv_data_storage()
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ I'm creating this data driven CV following the these tutorials:
 
 1. https://www.keanarichards.com/2020/10/18/how-to-make-your-resume-cv-in-r/
 2. http://nickstrayer.me/datadrivencv/
+3. https://annekelincoln.com/resume-in-r/
+
